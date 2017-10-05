31acc778085e4136a20d84584e36310f8eef50c4
VeroniqueZimmermann <zimmermannveronique@protonmail.com>
2017-10-05 09:22:05 +0200

    Zeichensetzung
    weitere Arrays und mit foreach Ausgabe

--- autoconfigurator.php
+++ autoconfigurator.php
@@ -1,11 +1,30 @@
 <?php
 $autoKonfiguration = 
-array('Hersteller','Bauart' => array('SUV','Kombi'),'Motor' => array('Benzin','Diesel','Elektro','Hybrid'));        // Bauart = Kombi, Cabrio // numerisch indiziertes eindimensionales Array
+array('Hersteller'=> array('VW', 'BMW', 'FORD'),
+        'Bauart' => array('SUV', 'Kombi', 'Cabrio'), 
+        'Motor' => array('Benzin', 'Diesel', 
+                        'Elektro' /*=> array('Trocken','Brennstoffzelle','Fusion')*/
+                    )
+    );        // Bauart = Kombi, Cabrio // numerisch indiziertes eindimensionales Array
 
-echo "<pre>"; 
+
+/* echo "<pre>"; 
 print_r($autoKonfiguration);
 echo "</pre>";
 echo "<hr>";
-echo $autoKonfiguration['Motor']['1'];      // der zweite Wert wird ausgegeben
+echo $autoKonfiguration['Motor'][1];      // der zweite Wert wird ausgegeben
+echo "<hr>"; */
+
+// zeige die erste Ebene des Arrays
+
+foreach($autoKonfiguration as $key => $inhalt)     // Zerlegung in Schlüssel und Inhalt // $bla as $key => $zeug - assoziatives array!
+{   // und direkt dahinter die passenden Inhalte
+    echo "$key -> ";
+    foreach($inhalt as $wert)                       // $bla as $key - eindimensionales
+    {
+        echo "$wert,";                              // wenn teils weitere Werte vorhanden, dann so, falls $wert noch weitere Informationen, dann andere Schleife
+    }
+        echo "<br>";
+}
 
 ?>
