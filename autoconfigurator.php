32d815c4c2a42f3b27b728e795737fad21f7f844
VeroniqueZimmermann <zimmermannveronique@protonmail.com>
2017-10-05 08:51:42 +0200

    Array hier und Array da
    wieder eine kleine Klärung und Spielerei mit Arrays

--- autoconfigurator.php
+++ autoconfigurator.php
@@ -1,11 +1,11 @@
 <?php
 $autoKonfiguration = 
-array('Hersteller','Bauart','Motor');        // Bauart = Kombi, Cabrio // numerisch indiziertes eindimensionales Array
+array('Hersteller','Bauart' => array('SUV','Kombi'),'Motor' => array('Benzin','Diesel','Elektro','Hybrid'));        // Bauart = Kombi, Cabrio // numerisch indiziertes eindimensionales Array
 
 echo "<pre>"; 
 print_r($autoKonfiguration);
 echo "</pre>";
 echo "<hr>";
-echo $autoKonfiguration[1];
+echo $autoKonfiguration['Motor']['1'];      // der zweite Wert wird ausgegeben
 
 ?>
